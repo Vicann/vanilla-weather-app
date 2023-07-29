c5e9523f28c3c7176564cb4eb724676b1fa96a92
Victoria <39576407+Vicann@users.noreply.github.com>
2023-07-29 12:48:40 -0400

    made new function, deleted chunk of code

--- src/app.js
+++ src/app.js
@@ -21,6 +21,32 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes} `;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class = "row">`;
+  let days = ["Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (days) {
+    forecastHTML =
+      forecastHTML +
+      `
+                
+                  <div class="col-3">
+                    <div class="weather-forecast-date">${days}</div>
+                    <img
+                      src="https://openweathermap.org/img/wn/03d@2x.png"
+                      alt=""
+                      width="36"
+                    />
+                    <div class="weather-forecast-temperatures">
+                      <span class="weather-forecast-temperature-max">18°</span>
+                      <span class="weather-forecast-temperature-min">12°</span>
+                    </div>
+                </div>`;
+  });
+  forecastHTML = forecastHTML + `</div`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -85,6 +111,7 @@ let celsiusLink = document.querySelector("#celsius-link");
 
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
+displayForecast();
 /*
 
 function currentPosition(position) {
